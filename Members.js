96a3af726cdb484ae8959fdef19bbd6f747c8b0f
z1chh <lolchen2012@gmail.com>
2022-11-18 23:08:38 -0500

    template

---
+++ Members.js
@@ -0,0 +1,23 @@
+// Create members
+const members = [
+    {
+        id: 1,
+        name: 'Jarad Higgins',
+        email: 'jah@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 2,
+        name: 'Charlton Howard',
+        email: 'laroi@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Taurus Bartlett',
+        email: 'polog@example.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;

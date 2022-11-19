476530ec22754220dd4f4aaf3ecd966e9f816487
z1chh <lolchen2012@gmail.com>
2022-11-18 23:37:36 -0500

    uh lowercase :D

--- Users.js
+++ Users.js
@@ -4,19 +4,19 @@ const users = [
         id: 1,
         name: 'Jarad Higgins',
         email: 'jah@gmail.com',
-        is_broker: False
+        is_broker: false
     },
     {
         id: 2,
         name: 'Charlton Howard',
         email: 'laroi@gmail.com',
-        is_broker: False
+        is_broker: false
     },
     {
         id: 3,
         name: 'Taurus Bartlett',
         email: 'polog@example.com',
-        is_broker: True
+        is_broker: true
     }
 ];
 

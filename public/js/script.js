ce1438fec46822ef0c9feb51560eeb6749b001da
Allybaitor <longkun_chen@hotmail.com>
2022-11-19 16:28:09 -0500

    login 7

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,17 @@
+document.querySelector("#submitButton").addEventListener("click", e => {
+    e.preventDefault();
+    // var username1 = document.getElementById("loginUser").value;
+    // var password1 = document.getElementById("loginPassword").value;
+    // if(username1=="admin" && password1=="user"){
+    //     alert("login success");
+    //     return false;
+    // }
+    // else{
+    //     alert("login fail");
+    // }
+    window.location.href = "carrier.html";
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginPage = document.querySelector("#login");
     const createPage = document.querySelector("#create");
@@ -15,30 +29,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
-
-// // Imports
-// const Job = require('../../server/model/Job');
-
-// // Hi
-// const create_post = document.getElementById("{POST-BUTTON}");
-// const find_post = document.getElementById("{FIND-POST-BUTTON}");
-
-// // Interactions
-// create_post.onclick = () => postJob();
-// find_post.onclick = () => findJob();
-
-// // Functions
-// const postJob = () =>
-// {
-//     // TODO
-// }
-
-// const findJob = () =>
-// {
-//     Post.findOne({_id: 123})
-//     .populate('postedBy')
-//     .exec(function(err, post)
-//     {
-//         // do stuff with post
-//     });}
 

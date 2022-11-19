0f76f69d7e6fa1520a1a07e2022ebfb9ae573b9e
z1chh <lolchen2012@gmail.com>
2022-11-19 18:34:52 -0500

    a

--- src/Load-In_Road/test/index.js
+++ src/Load-In_Road/test/index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const User = require("../../server/model/User");
+
+// Initialize express
 const app = express();
+
+// Set default port
+const PORT = process.env.PORT || 5000 ;
+
+// Connect to MongoDB
+const dbUri = "mongodb+srv://admin:penguin@penguins.uqphgd2.mongodb.net/Penguins?retryWrites=true&w=majority";
+mongoose.connect(dbUri) //, {useNewUrlParse: true, useUnifiedTopology: true})
+.then((res) => app.listen(PORT, () => console.log(`Server started on ${PORT}.`)))
+.catch((err) => console.log(err));
 
 app.post("/users", (req, res) =>
 {

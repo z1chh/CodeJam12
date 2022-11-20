75e4676629242718a924fef1c9cb8fd86372ed6a
z1chh <lolchen2012@gmail.com>
2022-11-20 00:16:14 -0500

    cleaning shit

---
+++ src/server/model/Users.js
@@ -0,0 +1,29 @@
+// Create users
+const users = [
+    {
+        id: 1,
+        username: 'Jarad Higgins',
+        email: 'jah@gmail.com',
+        is_shipper: false
+    },
+    {
+        id: 2,
+        username: 'Charlton Howard',
+        email: 'laroi@gmail.com',
+        is_shipper: false
+    },
+    {
+        id: 3,
+        username: 'Taurus Bartlett',
+        email: 'polog@example.com',
+        is_shipper: true
+    },
+    {
+        id: 4,
+        username: 'HandsomeLKC',
+        email: 'lk_c@hotmail.ca',
+        is_shipper: true
+    }
+];
+
+module.exports = users;

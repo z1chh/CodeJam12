b634ffa7e2239bf1d5783ce39b15cc6b2284efc0
z1chh <lolchen2012@gmail.com>
2022-11-20 10:43:51 -0500

    added password

--- src/Load-In_Road/public/js/tables1.js
+++ src/Load-In_Road/public/js/tables1.js
@@ -89,42 +89,49 @@ let USERS = [
         id: 1,
         username: 'Jarad_Higgins',
         email: 'jah@gmail.com',
+        password: 'pass123',
         is_shipper: false
     },
     {
         id: 2,
         username: 'Charlton_Howard',
         email: 'laroi@gmail.com',
+        password: 'pass123',
         is_shipper: false
     },
     {
         id: 3,
         username: 'Taurus_Bartlett',
         email: 'polog@example.com',
+        password: 'pass123',
         is_shipper: true
     },
     {
         id: 4,
         username: 'HandsomeLKC',
         email: 'lk_c@hotmail.ca',
+        password: 'pass123',
         is_shipper: true
     },
     {
         id: 5,
         username: 'Kalberto',
         email: 'spenguin@gmail.com',
+        password: 'pass123',
         is_shipper: true
     },
     {
         id: 6,
         username: 'JimmePrice',
         email: 'jimmyp@gmail.com',
+        password: 'pass123',
         is_shipper: false
     },
     {
         id: 7,
         username: 'Kadriel',
         email: 'sepenguin@gmail.com',
+        password: 'pass123',
         is_shipper: true
     }
 ];
